ab8bb9b982841dbfa65cfe40b25fbc02ea634d28
shamira <chuquimia>
2025-11-19 16:51:11 -0300

    Backend: Criação do server.js, configuração da rota GET e separação do arquivo dados.js

--- frontend/dist/js/front.js
+++ frontend/dist/js/front.js
@@ -6,26 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const filtersContainer = document.getElementById('filters');
     const btnLight = document.getElementById('btn-light-mode');
     const btnDarkMode = document.getElementById('btn-dark-mode');
-    const htmlElement = document.documentElement; 
+    const htmlElement = document.documentElement;
     const btnMobileMenu = document.getElementById('btn-mobile-menu');
-    const mobileMenu = document.getElementById('mobile-menu'); 
+    const mobileMenu = document.getElementById('mobile-menu');
 
 
     if (btnMobileMenu && mobileMenu) {
         btnMobileMenu.addEventListener('click', () => {
-            mobileMenu.classList.toggle('hidden'); 
+            mobileMenu.classList.toggle('hidden');
         });
     }
 
-//     if (btnLight && btnDark && htmlElement) {
-//     btnLight.addEventListener('click', () => {
-//         htmlElement.classList.remove('dark');
-//     });
+    //     if (btnLight && btnDark && htmlElement) {
+    //     btnLight.addEventListener('click', () => {
+    //         htmlElement.classList.remove('dark');
+    //     });
 
-//     btnDark.addEventListener('click', () => {
-//         htmlElement.classList.add('dark');
-//     });
-// }
+    //     btnDark.addEventListener('click', () => {
+    //         htmlElement.classList.add('dark');
+    //     });
+    // }
     const idToArea = {
         'filter-todos': 'Todos',
         'filter-tecnologia': 'Tecnologia e Inovação',
@@ -64,23 +64,23 @@ document.addEventListener('DOMContentLoaded', () => {
             const salarioFormatado = formatarSalario(profissao.mediaSalarial);
             const card = document.createElement('div');
             card.className = 'border border-gray-300 dark:border-gray-900 rounded-lg shadow-lg bg-white dark:bg-gray-900 overflow-hidden flex flex-col h-full ' +
-            'transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl';
+                'transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl';
             card.innerHTML = `
-                <img src="${profissao.imagem}" alt="${profissao.titulo}" class="w-full h-40 object-cover">
-                <div class="p-5 flex flex-col flex-1">
-                    <div class="flex-1 mb-4">
-                        <h3 class="text-2xl font-semibold text-slate-900 dark:text-gray-100 mb-3">${profissao.titulo}</h3>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class=" dark:text-gray-100 font-medium text-slate-800">Área:</span> ${profissao.area}</p>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Demanda:</span> ${profissao.demanda}</p>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300 line-clamp-2"><span class="font-medium text-slate-800  dark:text-gray-100">Descrição:</span> ${profissao.descricao}</p>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Competências:</span> ${profissao.competencia}</p>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Formação:</span> ${profissao.formacao}</p>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Média Salarial:</span> ${salarioFormatado}</p>
-                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Impacto:</span> ${profissao.impactoSocial}</p>
-                    </div>
-                    <a href="#" class="block w-full bg-[#A59CFF] py-2 text-white text-center rounded hover:bg-indigo-500 transition-colors duration-300 font-medium">Ver Detalhes</a>
-                </div>
-            `;
+                <img src="${profissao.imagem}" alt="${profissao.titulo}" class="w-full h-40 object-cover">
+                <div class="p-5 flex flex-col flex-1">
+                    <div class="flex-1 mb-4">
+                        <h3 class="text-2xl font-semibold text-slate-900 dark:text-gray-100 mb-3">${profissao.titulo}</h3>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class=" dark:text-gray-100 font-medium text-slate-800">Área:</span> ${profissao.area}</p>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Demanda:</span> ${profissao.demanda}</p>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300 line-clamp-2"><span class="font-medium text-slate-800  dark:text-gray-100">Descrição:</span> ${profissao.descricao}</p>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Competências:</span> ${profissao.competencia}</p>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Formação:</span> ${profissao.formacao}</p>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Média Salarial:</span> ${salarioFormatado}</p>
+                        <p class="text-sm text-slate-600 mb-2  dark:text-gray-300"><span class="font-medium text-slate-800  dark:text-gray-100">Impacto:</span> ${profissao.impactoSocial}</p>
+                    </div>
+                    <a href="#" class="block w-full bg-[#A59CFF] py-2 text-white text-center rounded hover:bg-indigo-500 transition-colors duration-300 font-medium">Ver Detalhes</a>
+                </div>
+            `;
             cardsContainer.appendChild(card);
         });
     }
